678d346106e594ce5429a77d28ea710b1eb7ebec
PCE1968 <erickson.paul@mayo.edu>
2022-02-09 22:11:48 -0600

    Deliverable 2 challenge 7

--- Data/PH-EmployeeDB-queries.sql
+++ Data/PH-EmployeeDB-queries.sql
@@ -123,6 +123,19 @@ SELECT ri.emp_no,
 	ri.first_name,
 	ri.last_name,
 	d.dept_name
+INTO sales_retires
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales')
+ORDER BY d.dept_name;
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	d.dept_name
 INTO sales_and_Dev_retires
 FROM retirement_info as ri
 INNER JOIN dept_emp AS de
---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,67 @@
+-- Query to create a Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
+
+-- Solution
+SELECT 
+	DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+-- Find the number of employees by their most recent job title who are about to retire.
+SELECT COUNT(*) COUNT, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(*) DESC;
+
+-- Find number of employees that are eligible for the mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+from employees as e
+join dept_emp as de
+on e.emp_no = de.emp_no
+join titles as ti
+on e.emp_no = ti.emp_no
+WHERE de.to_date = '9999-01-01'
+	AND e.birth_date BETWEEN '1965-01-01' and '1965-12-31'
+ORDER BY e.emp_no;
+
+-- Count of available mentors by title
+SELECT COUNT(*) COUNT, title
+INTO mentor_count
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY COUNT(*) DESC;
